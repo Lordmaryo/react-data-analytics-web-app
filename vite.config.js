833f5615500ca6d3996c3197521864ac75e85d8f
emmanuel ofoneta <emmanuelofoneta@gmail.com>
2024-05-09 14:46:47 +0100

    Added 'react-typed' to external dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
 
-// https://vitejs.dev/config/
-export default defineConfig({
+import react from '@vitejs/plugin-react';
+
+export default {
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['react-typed'], 
+    },
+  },
+};
